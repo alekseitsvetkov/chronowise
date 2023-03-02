785b8615d0ff457c09308d5cf5d71e0e2af40211
alekseytsvetkov <code.aleksey@icloud.com>
2023-03-03 01:32:07 +0300

    turbo

---
+++ .prettierrc.js
@@ -0,0 +1,27 @@
+/**
+ * {@type require('prettier').Config}
+ */
+module.exports = {
+	useTabs: true,
+	printWidth: 100,
+	singleQuote: true,
+	trailingComma: 'none',
+	bracketSameLine: false,
+	semi: true,
+	quoteProps: 'consistent',
+	importOrder: [
+		// external packages
+		'^([A-Za-z]|@[^s/])',
+    // app packages
+		'^@sd/(ui)(/.*)?$',
+		// this package
+		'^~/',
+		// relative
+		'^\\.'
+	],
+	importOrderSortSpecifiers: true,
+	importOrderParserPlugins: ['importAssertions', 'typescript', 'jsx'],
+	pluginSearchDirs: false,
+	plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+	tailwindConfig: 'packages/ui/tailwind.config.js'
+};
