fbfbac55ed8a2f6336f7565878412f63d0b36828
Aleksey Tsvetkov <code.aleksey@icloud.com>
2023-03-03 17:05:55 +0300

    ci

--- apps/desktop/src/constants/notification.ts
+++ apps/desktop/src/constants/notification.ts
@@ -1,3 +1,3 @@
-export const NOTIFICATION_TITLE = 'Pomodoro'
+export const NOTIFICATION_TITLE = 'karoshi'
 export const FOCUS_TIME_NOTIFICATION = 'Time to focus!'
 export const BREAK_TIME_NOTIFICATION = 'Time for a break!'
