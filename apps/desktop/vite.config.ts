5f20dac9b6cd3178f2b531f46fad1c56b56f6092
Aleksey Tsvetkov <code.aleksey@icloud.com>
2023-03-03 16:10:24 +0300

    ci

--- apps/desktop/vite.config.ts
+++ apps/desktop/vite.config.ts
@@ -1,9 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [tsconfigPaths(), react()],
+  css: {
+		modules: {
+			localsConvention: 'camelCaseOnly'
+		}
+	},
   root: 'src',
   // Vite options tailored for Tauri development and only applied in `tauri dev` or `tauri build`
   // prevent vite from obscuring rust errors
