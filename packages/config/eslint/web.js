f4314f3c6e761a71c4c37797dc2de49fc94c20e8
Aleksey Tsvetkov <code.aleksey@icloud.com>
2023-03-29 23:59:30 +0300

    updated

--- packages/config/eslint/web.js
+++ packages/config/eslint/web.js
@@ -1,18 +1,4 @@
 module.exports = {
-	compilerOptions: {
-		plugins: [
-      {
-        "name": "next"
-      }
-    ]
-	},
-	include: [
-    "next-env.d.ts",
-    "**/*.ts",
-    "**/*.tsx",
-    ".next/types/**/*.ts",
-    ".contentlayer/generated"
-  ],
 	extends: [require.resolve('./base.js')],
 	env: {
 		browser: true,
