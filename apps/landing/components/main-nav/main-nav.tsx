7f7daaf2ed142e457dc408ecdfecf969ec9f9873
alekseytsvetkov <code.aleksey@icloud.com>
2023-04-01 18:20:27 +0300

    landing - added backdrop fliter

--- apps/landing/components/main-nav/main-nav.tsx
+++ apps/landing/components/main-nav/main-nav.tsx
@@ -54,7 +54,7 @@ export function MainNav({ items }: MainNavProps) {
             variant="ghost"
             className="-ml-4 text-base hover:bg-transparent focus:ring-0 md:hidden"
           >
-            <Icons.logo className="mr-2 h-4 w-4" />{" "}
+            <Icons.logo className="mr-2 h-6 w-6" />
             <span className="font-bold">Menu</span>
           </Button>
         </DropdownMenuTrigger>
