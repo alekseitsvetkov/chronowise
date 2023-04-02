eefa730a268585272dc643291129cda0f263b006
alekseytsvetkov <code.aleksey@icloud.com>
2023-04-02 19:40:46 +0300

    landing - updated download link

--- apps/landing/utils/download-link.ts
+++ apps/landing/utils/download-link.ts
@@ -1,25 +1,20 @@
-import { getOs } from '@/utils';
+import {version} from '@chronowise/desktop/package.json'
 
-// WIP
-// https://github.com/USER/PROJECT/releases/latest/download/package.zip
 export const getDownloadLink = (osName: "Mac" | "Windows" | "Linux" | "iOS" | "Android") => {
   const user = "alekseytsvetkov";
   const project = "chronowise";
   const os = osName.toLowerCase();
-  const appVersion = "0.0.1";
-  const tag = "untagged-2401ad28de66c3606ec0";
 
-  // chronowise.app.tar.gz
   // chronowise_0.0.1_amd64.AppImage
   // chronowise_0.0.1_amd64.deb
   // chronowise_0.0.1_x64.dmg
   // chronowise_0.0.1_x64_en-US.msi
 
   const filename = {
-    mac: `chronowise_${appVersion}_x64.dmg`,
-    windows: `chronowise_${appVersion}_x64_en-US.msi`,
-    linux: `chronowise_${appVersion}_amd64.AppImage`,
+    mac: `chronowise_${version}_x64.dmg`,
+    windows: `chronowise_${version}_x64_en-US.msi`,
+    linux: `chronowise_${version}_amd64.AppImage`,
   }[os];
 
-  return `https://github.com/${user}/${project}/releases/download/${tag}/${filename}`;
+  return `https://github.com/${user}/${project}/releases/latest/download/${filename}`;
 };
