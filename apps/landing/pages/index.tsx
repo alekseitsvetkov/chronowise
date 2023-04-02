c28cee584dc766b5c924fb4c0d81927a8003a00e
alekseytsvetkov <code.aleksey@icloud.com>
2023-04-02 17:12:41 +0300

    landing - added section

--- apps/landing/pages/index.tsx
+++ apps/landing/pages/index.tsx
@@ -81,7 +81,7 @@ export default function Home() {
             width="300"
             height="231"
           />}
-          <div className="mx-auto flex flex-col items-center gap-4 px-4 2xl:mb-52 xl:mb-28 lg:mb-20 md:mb-18 sm:mb-20 mb-16">
+          <div className="mx-auto flex flex-col items-center gap-4 px-4 2xl:mb-40 xl:mb-28 lg:mb-20 md:mb-18 sm:mb-20 mb-16">
             <h1 className="fade-in-heading z-30 text-center text-black dark:text-white text-3xl font-bold leading-[1.1] tracking-tight sm:text-3xl md:text-6xl">
               A better way to improve your workflow
             </h1>
@@ -105,6 +105,12 @@ export default function Home() {
                     </span>
                   </span>
                 </Button>
+                {/* TODO: get version from packages.json desktop app */}
+                <div className="animation-delay-2 fade-in text-gray-400">
+                  {currentOs === "Mac" && "v0.0.1 | macOS 11+ | Install via Homebrew"}
+                  {currentOs === "Windows" && "v0.0.1 | Windows 10+ | Install via Scoop"}
+                  {currentOs === "Linux" && "v0.0.1 | Linux | Install via Snap"}
+                </div>
               </>
             )}
           </div>
@@ -116,10 +122,10 @@ export default function Home() {
             height="283"
           />
           <hr className="border-t border-t-gray-150 dark:border-t-gray-550 w-full" />
-          <section className="fade-in-down container grid justify-center gap-6 pt-48">
-            <div className="mx-auto flex flex-col gap-4 md:max-w-[52rem] items-center">
-              <h2 className="text-3xl font-bold leading-[1.1] tracking-tight sm:text-3xl md:text-6xl">
-                All the features you need
+          <section className="fade-in-down container grid justify-center gap-6 pt-48 pb-24">
+            <div className="mx-auto flex flex-col gap-4 md:max-w-[56rem] items-center">
+              <h2 className="text-3xl font-bold leading-[1.1] tracking-tight sm:text-3xl md:text-6xl text-center">
+                A simple and snappy focus timer
               </h2>
               <p className="leading-normal text-gray-500 dark:text-gray-200 sm:text-lg sm:leading-7 text-center">
                 Chronowise is a new cross-platform focus timer experience built on Next.js and written from 
@@ -127,7 +133,7 @@ export default function Home() {
                 the ground up with performance in mind.
               </p>
             </div>
-            <div className="grid justify-center gap-4 sm:grid-cols-2 md:max-w-[62rem] md:grid-cols-3">
+            <div className="grid justify-center gap-4 sm:grid-cols-2 md:max-w-[72rem] md:grid-cols-3">
               <div className="relative overflow-hidden">
                 <div className="flex h-[320px] flex-col p-6 text-gray-850 items-center">
                   {!!womanOnLaptopImage && 
@@ -140,8 +146,152 @@ export default function Home() {
                     />
                   }
                   <div className="space-y-2 text-center">
-                    <h3 className="font-bold text-gray-850 dark:text-white text-lg">Simple design</h3>
-                    <p className="text-base text-gray-850 dark:text-white">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">Improving concentration</h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
+                      Working in short intervals with breaks helps you keep your focus and concentrate on the task at hand.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="relative overflow-hidden">
+                <div className="flex h-[320px] flex-col p-6 text-gray-850 items-center">
+                  {!!womanOnLaptopImage && 
+                    <Image
+                      src={womanOnLaptopImage}
+                      className="animation-delay-2 fade-in mb-6 px-4"
+                      alt="woman on laptop"
+                      width="200"
+                      height="161"
+                    />
+                  }
+                  <div className="space-y-2 text-center">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">Stress management</h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
+                      Regular breaks allow you to relax and reduce the stress level associated with the task.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="relative overflow-hidden">
+                <div className="flex h-[320px] flex-col p-6 text-gray-850 items-center">
+                  {!!womanOnLaptopImage && 
+                    <Image
+                      src={womanOnLaptopImage}
+                      className="animation-delay-2 fade-in mb-6 px-4"
+                      alt="woman on laptop"
+                      width="200"
+                      height="161"
+                    />
+                  }
+                  <div className="space-y-2 text-center">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">
+                      Better time management
+                    </h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
+                      The technique helps you evaluate the time spent on a task, which can improve your productivity.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="relative overflow-hidden">
+                <div className="flex h-[320px] flex-col p-6 text-gray-850 items-center">
+                  {!!womanOnLaptopImage && 
+                    <Image
+                      src={womanOnLaptopImage}
+                      className="animation-delay-2 fade-in mb-6 px-4"
+                      alt="woman on laptop"
+                      width="200"
+                      height="161"
+                    />
+                  }
+                  <div className="space-y-2 text-center">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">
+                      Interrupting Procrastination
+                    </h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
+                      The pomodoro technique helps overcome procrastination by providing a focus on the task for a short period of time.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="relative overflow-hidden">
+                <div className="flex h-[320px] flex-col p-6 text-gray-850 items-center">
+                  {!!womanOnLaptopImage && 
+                    <Image
+                      src={womanOnLaptopImage}
+                      className="animation-delay-2 fade-in mb-6 px-4"
+                      alt="woman on laptop"
+                      width="200"
+                      height="161"
+                    />
+                  }
+                  <div className="space-y-2 text-center">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">
+                      Improved work discipline
+                    </h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
+                      Consistently completing tasks within a set time frame builds good work habits and improves self-organization.
+                    </p>
+                  </div>
+                </div>
+              </div>
+              <div className="relative overflow-hidden">
+                <div className="flex h-[320px] flex-col p-6 text-gray-850 items-center">
+                  {!!womanOnLaptopImage && 
+                    <Image
+                      src={womanOnLaptopImage}
+                      className="animation-delay-2 fade-in mb-6 px-4"
+                      alt="woman on laptop"
+                      width="200"
+                      height="161"
+                    />
+                  }
+                  <div className="space-y-2 text-center">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">
+                      Maintaining motivation
+                    </h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
+                      The pomodoro technique allows you to see your progress and successful completion of tasks, which helps to maintain motivation.
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="mx-auto flex flex-col gap-4 md:max-w-[56rem]">
+              <p className="leading-normal text-gray-500 dark:text-gray-200 sm:text-lg sm:leading-7 text-center">
+                Chronowise is available on a range of platforms, view our downloads page
+                <br/>
+                to find out if your device is supported!
+              </p>
+            </div>
+          </section>
+          <hr className="border-t border-t-gray-150 dark:border-t-gray-550 w-full" />
+          <section className="fade-in-down container grid justify-center gap-6 pt-24">
+            <div className="mx-auto flex flex-col gap-4 md:max-w-[56rem] items-center">
+              <h2 className="text-3xl font-bold leading-[1.1] tracking-tight sm:text-3xl md:text-6xl text-center">
+                All the features you need
+              </h2>
+              <p className="leading-normal text-gray-500 dark:text-gray-200 sm:text-lg sm:leading-7 text-center">
+                Chronowise is a new cross-platform focus timer experience built on Next.js and written from 
+                <br />
+                the ground up with performance in mind.
+              </p>
+            </div>
+            <div className="grid justify-center gap-4 sm:grid-cols-2 md:max-w-[72rem] md:grid-cols-3">
+              <div className="relative overflow-hidden">
+                <div className="flex h-[320px] flex-col p-6 text-gray-850 items-center">
+                  {!!womanOnLaptopImage && 
+                    <Image
+                      src={womanOnLaptopImage}
+                      className="animation-delay-2 fade-in mb-6 px-4"
+                      alt="woman on laptop"
+                      width="200"
+                      height="161"
+                    />
+                  }
+                  <div className="space-y-2 text-center">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">Simple design</h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
                       Chronowise comes with a minimal, easy-to-use and beautiful interface.
                     </p>
                   </div>
@@ -159,8 +309,8 @@ export default function Home() {
                     />
                   }
                   <div className="space-y-2 text-center">
-                    <h3 className="font-bold text-gray-850 dark:text-white text-lg">Optimized & Powerful</h3>
-                    <p className="text-base text-gray-850 dark:text-white">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">Optimized & Powerful</h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
                       Built with the latest technologies and best practices.
                     </p>
                   </div>
@@ -178,8 +328,8 @@ export default function Home() {
                     />
                   }
                   <div className="space-y-2 text-center">
-                    <h3 className="font-bold text-gray-850 dark:text-white text-lg">Lightweight</h3>
-                    <p className="text-base text-gray-850 dark:text-white">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">Lightweight</h3>
+                    <p className="text-base text-gray-500 dark:text-gray-200">
                       Chronowise is always at your disposal without the need of a dock icon.
                     </p>
                   </div>
@@ -197,10 +347,10 @@ export default function Home() {
                     />
                   }
                   <div className="space-y-2 text-center">
-                    <h3 className="font-bold text-gray-850 dark:text-white text-lg">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">
                       Built for your keyboard
                     </h3>
-                    <p className="text-base text-gray-850 dark:text-white">
+                    <p className="text-base text-gray-500 dark:text-gray-200">
                       Chronowise is built with keyboard shortcuts in mind.
                     </p>
                   </div>
@@ -218,10 +368,10 @@ export default function Home() {
                     />
                   }
                   <div className="space-y-2 text-center">
-                    <h3 className="font-bold text-gray-850 dark:text-white text-lg">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">
                       Open Source
                     </h3>
-                    <p className="text-base text-gray-850 dark:text-white">
+                    <p className="text-base text-gray-500 dark:text-gray-200">
                       Chronowise is open source
                       <br />
                       and free to use.
@@ -241,17 +391,17 @@ export default function Home() {
                     />
                   }
                   <div className="space-y-2 text-center">
-                    <h3 className="font-bold text-gray-850 dark:text-white text-lg">
+                    <h3 className="font-bold text-gray-500 dark:text-gray-200 text-lg">
                     And Much More!
                     </h3>
-                    <p className="text-base text-gray-850 dark:text-white">
+                    <p className="text-base text-gray-500 dark:text-gray-200">
                     {"Chronowise is a work in progress. We're adding new features all the time."}
                     </p>
                   </div>
                 </div>
               </div>
             </div>
-            <div className="mx-auto flex flex-col gap-4 md:max-w-[52rem]">
+            <div className="mx-auto flex flex-col gap-4 md:max-w-[56rem]">
               <p className="leading-normal text-gray-500 dark:text-gray-200 sm:text-lg sm:leading-7 text-center">
                 Chronowise is available on a range of platforms, view our downloads page
                 <br/>
