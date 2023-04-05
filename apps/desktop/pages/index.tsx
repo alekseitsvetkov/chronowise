4b005a28e2b5700a138cff5a4b70223425fc9cb4
Aleksey Tsvetkov <code.aleksey@icloud.com>
2023-04-05 10:51:39 +0300

    added resize window by page, need move to provider

--- apps/desktop/pages/index.tsx
+++ apps/desktop/pages/index.tsx
@@ -2,6 +2,7 @@
 
 import { App } from '@/components'
 import Head from 'next/head';
+import { useEffect, useState } from 'react';
 
 export const metadata = {
   title: 'Chronowise',
@@ -9,6 +10,21 @@ export const metadata = {
 }
 
 export default function Home() {
+  const [tauriWindow, setTauriWindow] = useState<any>();
+
+  async function setupAppWindow() {
+		const tauriWindow = (await import('@tauri-apps/api/window'));
+		setTauriWindow({appWindow: tauriWindow.appWindow, LogicalSize: tauriWindow.LogicalSize});
+	}
+
+  useEffect(() => {
+    setupAppWindow();
+  }, [])
+
+  useEffect(() => {
+    tauriWindow && tauriWindow.appWindow.setSize(new tauriWindow.LogicalSize(300, 240));
+  }, [tauriWindow])
+  
   return (
     <>
       <Head>
--- apps/desktop/pages/settings/index.tsx
+++ apps/desktop/pages/settings/index.tsx
@@ -1,8 +1,8 @@
 "use client"
 
-import { App } from '@/components'
 import Head from 'next/head';
 import { useRouter } from 'next/router';
+import { useEffect, useState } from 'react';
 
 export const metadata = {
   title: 'Chronowise',
@@ -11,7 +11,21 @@ export const metadata = {
 
 export default function Home() {
   const router = useRouter()
-  
+  const [tauriWindow, setTauriWindow] = useState<any>();
+
+  async function setupAppWindow() {
+		const tauriWindow = (await import('@tauri-apps/api/window'));
+		setTauriWindow({appWindow: tauriWindow.appWindow, LogicalSize: tauriWindow.LogicalSize});
+	}
+
+  useEffect(() => {
+    setupAppWindow();
+  }, [])
+
+  useEffect(() => {
+    tauriWindow && tauriWindow.appWindow.setSize(new tauriWindow.LogicalSize(600, 500));
+  }, [tauriWindow])
+
   return (
     <>
       <Head>
