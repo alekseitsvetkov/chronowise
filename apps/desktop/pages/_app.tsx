33ee62241ced80dffa4d4796e8bb6207a0c766cf
Aleksey Tsvetkov <code.aleksey@icloud.com>
2023-04-07 15:08:35 +0300

    suspence

--- apps/desktop/pages/_app.tsx
+++ apps/desktop/pages/_app.tsx
@@ -2,12 +2,13 @@ import type { AppProps } from 'next/app'
 
 import '@chronowise/ui/style'
 import { WindowTitlebar } from '@/components'
+import { Suspense } from 'react'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <Suspense>
       <WindowTitlebar />
       <Component {...pageProps} />
-    </>
+    </Suspense>
   )
 }
